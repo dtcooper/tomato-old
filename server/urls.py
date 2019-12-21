2456cf985cb0a6112da4936ccb2fd478909026df
David Cooper <david.cooper@burningman.org>
2019-12-21 11:03:54 -0800

    Clean up material-admin stuff

--- server/urls.py
+++ server/urls.py
@@ -6,14 +6,12 @@ from django.utils.html import format_html
 from django.templatetags.static import static as static_url
 
 
-icon_url = static_url("admin/images/tomato.png")
+icon_url = static_url('admin/images/tomato.png')
 logo_img = '<img src="{url}" style="height: 40px; width: 40px; image-rendering: pixelated;">'
 
 admin.site.site_url = None
 admin.site.site_title = admin.site.site_header = format_html(
     logo_img + ' Tomato Radio Automation ' + logo_img, url=icon_url)
-admin.site.show_themes = True
-admin.site.favicon = icon_url
 
 urlpatterns = [
     path('', include('server.urls')),
