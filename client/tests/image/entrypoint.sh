9ae806bfb69f1daf2d98443d98f7ac1861ba4689
David Cooper <david.cooper@burningman.org>
2020-03-05 21:28:27 -0800

    Prefill server in tomato config :)

--- client/tests/image/entrypoint.sh
+++ client/tests/image/entrypoint.sh
@@ -4,8 +4,16 @@
 /venv/server/bin/python /app/server/manage.py sqlflush \
     | /venv/server/bin/python /app/server/manage.py dbshell  > /dev/null
 cat <<'EOF' | /venv/server/bin/python /app/server/manage.py shell
+import json
+import os
+
 from django.contrib.auth.models import User
+
 User.objects.create_superuser(username="test", password="test")
+
+os.mkdir('/root/.tomato')
+json.dump({'hostname': 'localhost:8000', 'protocol': 'http'},
+          open('/root/.tomato/config.json', 'w'))
 EOF
 
 supervisord -c /etc/supervisor/supervisord.conf
