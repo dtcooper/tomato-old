d88c3c01ec8224b77eb0560f1e658acb3956b09f
David Cooper <david.cooper@burningman.org>
2020-03-09 21:45:46 -0700

    Add DMG creater for macOS

--- client/build-mac.sh
+++ client/build-mac.sh
@@ -16,3 +16,22 @@ pyinstaller \
 # High res mode on Retina displays
 plutil -insert NSPrincipalClass -string NSApplication dist/run.app/Contents/Info.plist
 mv -v dist/run.app dist/Tomato.app
+rm -rf dist/run
+
+# If we have create-dmg
+if which create-dmg > /dev/null
+then
+    create-dmg \
+            --app-drop-link 200 0 \
+            --icon Tomato.app 0 0 \
+            --icon-size 100  \
+            --volicon tomato.icns \
+            --volname Tomato \
+            --window-pos 200 120 \
+            --window-size 450 100 \
+        Tomato.dmg dist
+    mv -v Tomato.dmg dist
+else
+    cd dist
+    zip -9r tomato.zip Tomato.app
+fi
